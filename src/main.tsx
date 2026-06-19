44552522d40c675eb66c52d62ab7459082d276d0
Craig Chihururu <craigchihururu@gmail.com>
2026-06-19 19:46:27 +0200

    Fix Vercel static loader routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { ViteReactSSG } from 'vite-react-ssg'
 import './index.css'
 import { routes } from './routes'
 
-export const createRoot = ViteReactSSG({ routes })
+export const createRoot = ViteReactSSG({
+  routes,
+  basename: import.meta.env.BASE_URL,
+})
